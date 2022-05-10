de6d47a482d49bcfdc71fdb7bc7e5cfd56e30ff2
Renato Mateus <marks_renato00@protonmail.com>
2022-05-10 03:36:03 -0300

    Atualizando jogo da velha

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import velha_funcoes
 
 board = velha_funcoes.new_game()
 player = 1
-played = []
-win = 0
+played = [] # cada jogada será cadastrada aqui, para evitar jogadas repetidas
+win = 0 # var que verifica se o jogo acabou
 max_played = 9
 
 
-while True:
+while max_played > 0:
     velha_funcoes.print_board(board, player)
     if win != 0:
         if win == 1:
@@ -17,15 +17,18 @@ while True:
             print("\033[31mPlayer 2 venceu!\033[m")
             break
     play = int(input("Player {}, enter a number: ".format(player)))
+
     if play not in played:
         played.append(play)
         board = velha_funcoes.shot(board, player, play)
         player += 1
+        max_played -= 1
     else:
         print("\033[31mJogada inválida\033[m")
         continue
+
     if player > 2:
         player = 1
     win = velha_funcoes.win(board)
-
-
+else:
+    print("\033[33m----- Empatou ------\033[m")
